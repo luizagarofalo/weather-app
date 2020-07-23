5ebf4561b3b81b454d888c32b2218045e15622aa
Luiza Garofalo <luiza.garofalo@gmail.com>
2020-07-23 00:18:29 -0300

    Add collection view

---
+++ weather-app/Sections/Hub/View/CurrentWeatherCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CurrentWeatherCollectionViewCell.swift
+//  weather-app
+//
+//  Created by Luiza Collado Garofalo on 22/07/20.
+//  Copyright © 2020 Luiza Collado Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- weather-app/Sections/Hub/View/HubViewController.swift
+++ weather-app/Sections/Hub/View/HubViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class HubViewController: UIViewController {
     
+    // MARK: - Outlets
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Properties
     var viewModel: HubViewModel!
     
+    // MARK: - Initializers
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -21,8 +26,33 @@ class HubViewController: UIViewController {
         self.viewModel = viewModel
     }
 
+    // MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
         viewModel.startLocationManager()
+    }
+    
+    // MARK: - Private Methods
+    private func setupCollectionView() {
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.collectionView.alwaysBounceVertical = true
+        self.collectionView.register(UINib(nibName: "CurrentWeatherCollectionViewCell", bundle: nil),
+                                     forCellWithReuseIdentifier: "CurrentWeatherCollectionViewCell")
+    }
+}
+
+extension HubViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "CurrentWeatherCollectionViewCell", for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.size.width, height: 375)
     }
 }
